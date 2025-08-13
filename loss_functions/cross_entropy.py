51cb1085276f6cc5fc9a68264b3a406f317ed87c
Shiraishi-Shodai <siranosuke1227@gmail.com>
2025-08-14 00:59:55 +0900

    交差エントロピーに関するJupyterノートブックとPythonスクリプトを削除

--- loss_functions/cross_entropy.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import torch
-from matplotlib import pyplot as plt
-import japanize_matplotlib
-import pandas as pd
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
-import torchvision.transforms.functional as F
